9979452cf9ad0711fc3a939a1c19f74f67cb0309
dp-arhipov <72319745+dp-arhipov@users.noreply.github.com>
2021-06-15 11:35:22 +0300

    Удаление лишних файлов

--- app/js/header.js
+++
@@ -1,7 +0,0 @@
-const url = window.location.href;
-$('.header .navbar-nav .nav-link').each((el) => {
-  $(el).removeClass('active');
-  if (el.href === url) {
-    $(el).addClass('active');
-  }
-});
